7b063910704b74bf8e00298e549eeab330ffd1bd
Ross <camden.ross@stu.fayette.kyschools.us>
2017-09-13 09:49:09 -0400

    Initial commit

---
+++ app/src/main/java/compsci/smallbusiness/SmallBusiness.java
@@ -0,0 +1,13 @@
+package compsci.smallbusiness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmallBusiness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_small_business);
+    }
+}
